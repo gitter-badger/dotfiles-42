3786487c50b2b18470cc3f05b9204db2af7bf6fb
lyuich <qii3hx@gmail.com>
2016-02-05 18:58:24 +0900

    initial commit

---
+++ .zsh/hash.zsh
@@ -0,0 +1,4 @@
+hash -d monstar=~/Applications/monstar-lab
+hash -d qii3hx=~/Applications/qii3hx
+hash -d shakr=~/Applications/shakr
+
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+DOT_DIRECTORY="${HOME}/dotfiles"
+
+cd ${DOT_DIRECTORY}
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
+
