14a3bdb037d987310d757ef22a5ee948b5d66b53
lyuich <qii3hx@gmail.com>
2016-04-05 12:10:09 +0900

    add ciopen() and .atom

---
+++ .zsh/ci.zsh
@@ -0,0 +1,11 @@
+ciopen() {
+  local commit result
+  commit="$1"
+  result="$(hub ci-status -v "$commit")"
+  if [[ $? == 3 ]]; then
+     echo $result
+   else
+    open "$(echo $result | awk '{print $2}')"
+   fi
+}
+
